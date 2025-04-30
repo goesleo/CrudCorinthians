052d8a501b56e0ddd3325286235b3d29043c73fa
Leonardo Goes <leonardotaqua20@hotmail.com>
2025-04-29 22:46:25 -0300

    Repository

--- src/main/java/com/example/myprojectcorinthians/repository/JogadorRepository.java
+++ src/main/java/com/example/myprojectcorinthians/repository/JogadorRepository.java
@@ -3,10 +3,11 @@ package com.example.myprojectcorinthians.repository;
 import com.example.myprojectcorinthians.entity.Jogador;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Component
+@Repository
 public interface JogadorRepository extends JpaRepository<Jogador, Long> {
 
     List<Jogador> findByPosicaoIgnoreCase(String posicao);
